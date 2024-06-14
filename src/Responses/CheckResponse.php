ba7ce11f2a4dd5bc67287234fa230c050b5d2002
Marc <marc@bhosted.nl>
2024-06-14 13:43:51 +0200

    Added CheckResponse class for handling response from check command

--- src/Responses/CheckResponse.php
+++ src/Responses/CheckResponse.php
@@ -4,11 +4,20 @@ namespace Mvdgeijn\BNamed\Responses;
 
 class CheckResponse extends Response
 {
+    const REGISTERED = '1';
+    const UNREGISTERED = '0';
+
     public array $data = [];
     public function __construct( \SimpleXMLElement $element )
     {
         foreach( $element->Result->children() as $result ) {
-            $this->data[] = $result;
+            $this->data[$result->SLD . '.' . $result->TLD] = [
+                'sld' => (string)$result->SLD,
+                'tld' => (string)$result->TLD,
+                'domain' => $result->SLD .'.' .$result->TLD,
+                'code' => (string)$result->AvailabilityCode,
+                'message' => (string)$result->AvailabilityText,
+            ];
         }
     }
 }

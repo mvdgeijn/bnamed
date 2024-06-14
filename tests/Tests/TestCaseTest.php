433874ca86739c4588c77389429bbcc348ba5379
Marc <marc@bhosted.nl>
2024-06-14 14:03:39 +0200

    Tests assertions

--- tests/Tests/TestCaseTest.php
+++ tests/Tests/TestCaseTest.php
@@ -27,14 +27,14 @@ class TestCaseTest extends TestCase
 
         $this->assertTrue( count( $result->data ) == 3, 'Number of results is not 3' );
 
-        $this->assertTrue( isset( $result->data['yahoo.com'] ) == true, 'Result for yahoo.com not in result data');
-        $this->assertTrue( $result->data['yahoo.com']['sld'] == "yahoo", 'sld does not contain yahoo' );
-        $this->assertTrue( $result->data['yahoo.com']['tld'] == "com", 'tld does not contain com' );
+        $this->assertTrue( isset( $result->data['yahoo.com'] ) === true, 'Result for yahoo.com not in result data');
+        $this->assertTrue( $result->data['yahoo.com']['sld'] === "yahoo", 'sld does not contain yahoo' );
+        $this->assertTrue( $result->data['yahoo.com']['tld'] === "com", 'tld does not contain com' );
         $this->assertTrue( $result->data['yahoo.com']['code'] == CheckResponse::REGISTERED, 'status is not registered' );
 
-        $this->assertTrue( isset( $result->data['yahoo-5391342341.com'] ) == true, 'Result for yahoo-5391342341.com not in result data' );
-        $this->assertTrue( $result->data['yahoo-5391342341.com']['sld'] == "yahoo-5391342341", 'sld does not contain yahoo-5391342341' );
-        $this->assertTrue( $result->data['yahoo-5391342341.com']['tld'] == "com", 'tld does not contain com' );
+        $this->assertTrue( isset( $result->data['yahoo-5391342341.com'] ) === true, 'Result for yahoo-5391342341.com not in result data' );
+        $this->assertTrue( $result->data['yahoo-5391342341.com']['sld'] === "yahoo-5391342341", 'sld does not contain yahoo-5391342341' );
+        $this->assertTrue( $result->data['yahoo-5391342341.com']['tld'] === "com", 'tld does not contain com' );
         $this->assertTrue( $result->data['yahoo-5391342341.com']['code'] == CheckResponse::UNREGISTERED, 'status is not unregistered' );
 
         $this->assertTrue( isset( $result->data['1.com'] ) );

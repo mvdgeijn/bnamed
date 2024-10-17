68b2c5bf88ec16145fa8a83a410d5a20c9fc5923
Marc <marc@bhosted.nl>
2024-10-17 15:50:33 +0200

    Recreated CheckResponse to use the Response base class

--- src/Responses/CheckResponse.php
+++ src/Responses/CheckResponse.php
@@ -2,22 +2,29 @@
 
 namespace Mvdgeijn\BNamed\Responses;
 
-class CheckResponse
+use SimpleXMLElement;
+
+class CheckResponse extends Response
 {
     const REGISTERED = '1';
     const UNREGISTERED = '0';
 
-    public array $data = [];
-    public function __construct( \SimpleXMLElement $element )
+    protected function parseResult(SimpleXMLElement $result): Response
     {
-        foreach( $element->Result->children() as $result ) {
-            $this->data[$result->SLD . '.' . $result->TLD] = [
-                'sld' => (string)$result->SLD,
-                'tld' => (string)$result->TLD,
-                'domain' => $result->SLD .'.' .$result->TLD,
-                'code' => (string)$result->AvailabilityCode,
-                'message' => (string)$result->AvailabilityText,
-            ];
+        foreach( $result->children() as $checkResult ) {
+            $this->items->push(
+                [
+                    'sld' => (string)$checkResult->SLD,
+                    'tld' => (string)$checkResult->TLD,
+                    'domain' => $checkResult->SLD . '.' . $checkResult->TLD,
+                    'code' => (string)$checkResult->AvailabilityCode,
+                    'message' => (string)$checkResult->AvailabilityText
+                ]
+            );
         }
+
+        $this->items->keyBy( 'domain' );
+
+        return $this;
     }
 }
